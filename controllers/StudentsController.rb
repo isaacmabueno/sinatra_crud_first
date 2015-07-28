c51bc0d2c45bad5a009fc72c627d294faa5d00cc
isaacmabueno <isaacmabueno@gmail.com>
2015-07-28 15:40:58 -0500

    update

--- controllers/StudentsController.rb
+++ controllers/StudentsController.rb
@@ -3,7 +3,8 @@ class StudentsController < ApplicationController
   #GET local host
   get '/' do
     #get all the students!
-    students = Students.all.to_json
+    @students = Students.all
+    erb :students_index
   end
 
 end
